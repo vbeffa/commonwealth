b43b94bd99df55a9b5da101e40e62e185daabd93
Vlad Beffa <vbeffa@mac.com>
2021-02-16 17:56:31 -0600

    add test_merkle_proof_depth_3

--- src/main.rs
+++ src/main.rs
@@ -144,49 +144,78 @@ mod tests {
     #[test]
     fn test_merkle_depth_0() -> Result<(), String> {
         let mut mt = MerkleTree::new(0, 4506850079084802999);
+
         mt.add_data(&String::from("foo"));
-        println!("{:#?}", mt);
+
         assert_eq!(mt.tree[0][0].hash, mt.root_hash);
+
         Ok(())
     }
 
     #[test]
     fn test_merkle_depth_1() -> Result<(), String> {
         let mut mt = MerkleTree::new(1, 17075777630381501106);
+
         mt.add_data(&String::from("foo"));
         mt.add_data(&String::from("bar"));
-        println!("{:#?}", mt);
+
         assert_eq!(mt.tree[0][0].hash, mt.root_hash);
+
         Ok(())
     }
 
     #[test]
     fn test_merkle_proof_depth_1() -> Result<(), String> {
         let mut mt = MerkleTree::new(1, 17075777630381501106);
+
         mt.add_data(&String::from("foo"));
         mt.add_data(&String::from("bar"));
-        println!("{:#?}", mt.generate_proof(0));
+
         assert_eq!(mt.generate_proof(0), [1]);
         assert_eq!(mt.generate_proof(1), [0]);
+
         Ok(())
     }
 
     #[test]
     fn test_merkle_proof_depth_2() -> Result<(), String> {
         let mut mt = MerkleTree::new(2, 4778819754073447529);
+
         mt.add_data(&String::from("foo"));
         mt.add_data(&String::from("bar"));
         mt.add_data(&String::from("baz"));
         mt.add_data(&String::from("yup"));
-        println!("{:#?}", mt);
-        println!("{:#?}", mt.generate_proof(0));
-        println!("{:#?}", mt.generate_proof(1));
-        println!("{:#?}", mt.generate_proof(2));
-        println!("{:#?}", mt.generate_proof(3));
+
         assert_eq!(mt.generate_proof(0), [1, 1]);
         assert_eq!(mt.generate_proof(1), [1, 0]);
         assert_eq!(mt.generate_proof(2), [0, 3]);
         assert_eq!(mt.generate_proof(3), [0, 2]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_merkle_proof_depth_3() -> Result<(), String> {
+        let mut mt = MerkleTree::new(3, 1556255166675498662);
+
+        mt.add_data(&String::from("foo"));
+        mt.add_data(&String::from("bar"));
+        mt.add_data(&String::from("baz"));
+        mt.add_data(&String::from("yup"));
+        mt.add_data(&String::from("maw"));
+        mt.add_data(&String::from("wap"));
+        mt.add_data(&String::from("pit"));
+        mt.add_data(&String::from("fos"));
+
+        assert_eq!(mt.generate_proof(0), [1, 1, 1]);
+        assert_eq!(mt.generate_proof(1), [1, 1, 0]);
+        assert_eq!(mt.generate_proof(2), [1, 0, 3]);
+        assert_eq!(mt.generate_proof(3), [1, 0, 2]);
+        assert_eq!(mt.generate_proof(4), [0, 3, 5]);
+        assert_eq!(mt.generate_proof(5), [0, 3, 4]);
+        assert_eq!(mt.generate_proof(6), [0, 2, 7]);
+        assert_eq!(mt.generate_proof(7), [0, 2, 6]);
+
         Ok(())
     }
 }
